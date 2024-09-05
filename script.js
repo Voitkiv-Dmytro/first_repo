9eb1c4da686c25cf89fea8802dbc38673b41bcab
Voitkiv-Dmytro <90179658+Voitkiv-Dmytro@users.noreply.github.com>
2024-09-05 19:04:55 +0300

    first commit
    add index.html
    add script.js
    add style.css

---
+++ script.js
@@ -0,0 +1,33 @@
+/*console.log(10>5)
+console.log(9<5)
+let a = "123"
+let b = 123
+console.log(a===b)
+
+let c = 110
+if (c>120){
+    console.log("Ваше число є більшим за 120")
+}else{
+    console.log("Число менше за 120")
+}
+
+const userPassword =prompt("Enter password");
+const validPassword = '1234';
+if (userPassword == validPassword) {
+    console.log('Доступ надано');
+} else {
+    console.log('Невірний пароль');
+}  
+
+let profession = prompt("Вкажи свою професію");
+if (profession == 'Лікар') {
+    alert('Дякуємо, що лікуєте.');
+} else if (profession == 'Пожежник') {
+    alert('Дякуємо, що рятуєте.');
+} else if (profession == 'Пекар') {
+    alert('Дякуємо, що годуєте.');
+} else if (profession == 'Водій') {
+    alert('Дякуємо, що возите.');
+} else {
+    alert('Я знаю багато професій');
+} */
